2449daaf1dfd2696dc4c0ae66295667d67eabdd9
GasparFernandez <gaspachofernandez@outlook.com>
2022-04-06 12:47:44 -0300

    functions

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -27,14 +27,17 @@ const nuevoModulo = 21 % 5 === 1;
 
 function devolverString(str) {
   // "Return" la string provista: str
-  // Tu código:
+  // Tu código: 
+  return "Hola";
   
 }
 
 function suma(x, y) {
   // "x" e "y" son números
   // Suma "x" e "y" juntos y devuelve el valor
-  // Tu código:
+  // Tu código: 
+  console.log(x + y);
+  return suma;
   
 }
 
